0a901e0ec6ac5d6bd6e0b4d6657b89f104597597
crime <139165956+dayrooms@users.noreply.github.com>
2026-09-08 01:35:06 -0500

    Update advice.js

--- commands/advice.js
+++ commands/advice.js
@@ -1,5 +1,4 @@
-
-const{ MessageEmbed } = require('discord.js');
+const{ EmbedBuilder } = require('discord.js');
 const axios = require('axios')
 const { default_prefix ,color,error,owner } = require("../config.json")
 const talkedRecently = new Set();
@@ -23,7 +22,7 @@ module.exports = {
       
 .then(response => {
       
-  let embed = new MessageEmbed()
+  let embed = new EmbedBuilder()
     .setDescription('\```' + response.data.advice + '\```')
     .setColor(color)
            message.reply({embeds:[embed]})
