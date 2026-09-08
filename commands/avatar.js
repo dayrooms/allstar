e5ef5734081df5fd9306b6dae0771864fcd794b3
crime <139165956+dayrooms@users.noreply.github.com>
2026-09-08 01:39:53 -0500

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,5 +1,4 @@
-
-const{ MessageEmbed,MessageButton,MessageActionRow } = require('discord.js');
+const{ EmbedBuilder,ButtonBuilder,ActionRowBuilder,ButtonStyle } = require('discord.js');
 const { default_prefix ,color,error,owner } = require("../config.json")
 const talkedRecently = new Set();
 module.exports = {
@@ -28,60 +27,60 @@ module.exports = {
 
   
 try {
-         const row = new MessageActionRow()
+         const row = new ActionRowBuilder()
         .addComponents(
-         new MessageButton()
+         new ButtonBuilder()
          .setLabel('webp')
           .setEmoji("<:MessageLink:1010885859735785553>")
-         .setURL(`${mentionedMember.displayAvatarURL({format: "webp", dynamic: true, size: 4096})}`)
-         .setStyle('LINK'),
+         .setURL(`${mentionedMember.displayAvatarURL({extension: "webp", size: 4096})}`)
+         .setStyle(ButtonStyle.Link),
         )
         .addComponents(
-         new MessageButton()
+         new ButtonBuilder()
          .setLabel('jpg')
           .setEmoji("<:MessageLink:1010885859735785553>")
-           .setURL(`${mentionedMember.displayAvatarURL({format: "jpg", dynamic: true, size: 4096})}`)
-         .setStyle('LINK'),
+           .setURL(`${mentionedMember.displayAvatarURL({extension: "jpg", size: 4096})}`)
+         .setStyle(ButtonStyle.Link),
         )
         .addComponents(
-         new MessageButton()
+         new ButtonBuilder()
          .setLabel('png')
           .setEmoji("<:MessageLink:1010885859735785553>")
-          .setURL(`${mentionedMember.displayAvatarURL({format: "png", dynamic: true, size: 4096})}`)
-           .setStyle('LINK'),
+          .setURL(`${mentionedMember.displayAvatarURL({extension: "png", size: 4096})}`)
+           .setStyle(ButtonStyle.Link),
           )
          
-          let embed = new MessageEmbed()
-          .setImage((mentionedMember.displayAvatarURL({ format: "png", dynamic: true, size: 4096 })))
+          let embed = new EmbedBuilder()
+          .setImage((mentionedMember.displayAvatarURL({ extension: "png", size: 4096 })))
           .setFooter({ text: `${mentionedMember.user.tag}`})
           .setColor(color)
           await message.reply({embeds:[embed] , components: [row]}).catch(() => {/*Ignore error*/})
         }catch{
         
-            const row = new MessageActionRow()
+            const row = new ActionRowBuilder()
       .addComponents(
-       new MessageButton()
+       new ButtonBuilder()
        .setLabel('webp')
         .setEmoji("<:MessageLink:1010885859735785553>")
-       .setURL(`${message.author.displayAvatarURL({format: "webp", dynamic: true, size: 4096})}`)
-       .setStyle('LINK'),
+       .setURL(`${message.author.displayAvatarURL({extension: "webp", size: 4096})}`)
+       .setStyle(ButtonStyle.Link),
       )
       .addComponents(
-       new MessageButton()
+       new ButtonBuilder()
        .setLabel('jpg')
         .setEmoji("<:MessageLink:1010885859735785553>")
-         .setURL(`${message.author.displayAvatarURL({format: "jpg", dynamic: true, size: 4096})}`)
-       .setStyle('LINK'),
+         .setURL(`${message.author.displayAvatarURL({extension: "jpg", size: 4096})}`)
+       .setStyle(ButtonStyle.Link),
       )
       .addComponents(
-       new MessageButton()
+       new ButtonBuilder()
        .setLabel('png')
         .setEmoji("<:MessageLink:1010885859735785553>")
-        .setURL(`${message.author.displayAvatarURL({format: "png", dynamic: true, size: 4096})}`)
-       .setStyle('LINK'),
+        .setURL(`${message.author.displayAvatarURL({extension: "png", size: 4096})}`)
+       .setStyle(ButtonStyle.Link),
       )
-        let embed = new MessageEmbed()
-        .setImage((message.author.displayAvatarURL({ format: "png", dynamic: true, size: 4096 })))
+        let embed = new EmbedBuilder()
+        .setImage((message.author.displayAvatarURL({ extension: "png", size: 4096 })))
         .setFooter({ text: `${message.author.tag}`})
         .setColor(color)
         await message.reply({embeds : [embed] , components: [row]}).catch(() => {/*Ignore error*/})
