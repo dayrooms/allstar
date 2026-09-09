b99fce2ba72f0dda0db55f11e4a8df0b0503460a
crime <139165956+dayrooms@users.noreply.github.com>
2026-09-09 01:05:54 -0500

    Update tags.js

--- commands/tags.js
+++ commands/tags.js
@@ -1,5 +1,4 @@
-
-const{ MessageEmbed,MessageButton, MessageActionRow } = require('discord.js');
+const{ EmbedBuilder,ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
 const db = require('quick.db')
 const { default_prefix ,color,error,owner,xmark } = require("../config.json")
 const talkedRecently = new Set();
@@ -26,7 +25,7 @@ module.exports = {
                 const authorized = [
           owner
         ];
-   if (!authorized.includes(message.author.id)) return message.reply({embeds:[new MessageEmbed().setDescription(`${xmark} This is a booster only command!`).setColor(error)]})
+   if (!authorized.includes(message.author.id)) return message.reply({embeds:[new EmbedBuilder().setDescription(`${xmark} This is a booster only command!`).setColor(error)]})
       
       }
       
@@ -48,22 +47,22 @@ module.exports = {
           .join("\n");
 
 
-               const button1 = new MessageButton()
+               const button1 = new ButtonBuilder()
       .setCustomId('previousbtn')
       .setEmoji("<a:left:1033526539188449310>")
-      .setStyle('PRIMARY');
-            let invite2 = new MessageButton()
+      .setStyle(ButtonStyle.Primary);
+            let invite2 = new ButtonBuilder()
          .setLabel('Invite')
          .setURL("https://discord.com/api/oauth2/authorize?client_id=938863295543251024&permissions=8&scope=bot%20applications.commands")
-         .setStyle('LINK')
-      const button2 = new MessageButton()
+         .setStyle(ButtonStyle.Link)
+      const button2 = new ButtonBuilder()
       .setCustomId('nextbtn')
       .setEmoji("<:right:1033526800401317928> ")
-      .setStyle('PRIMARY');
-       const button3 = new MessageButton()
+      .setStyle(ButtonStyle.Primary);
+       const button3 = new ButtonBuilder()
       .setCustomId('fastp')
       .setEmoji("<:DW_X_Mark:1032345318127304806> ")
-      .setStyle('DANGER');
+      .setStyle(ButtonStyle.Danger);
 
 
 
@@ -72,8 +71,8 @@ module.exports = {
       button3,
       button2,
   ]
-    const row = new MessageActionRow().addComponents(buttonList);
-        let embed = new MessageEmbed()
+    const row = new ActionRowBuilder().addComponents(buttonList);
+        let embed = new EmbedBuilder()
 
         .setColor(color)
        // .setThumbnail(message.guild.iconURL({dynamic:true,size:4096}))
@@ -123,7 +122,7 @@ const collector = await curPage.createMessageComponentCollector({
       
                 embed
                   .setFooter(
-                    `Page - ${page}/${Math.round(data.length / 10 + 1)}`
+                    { text: `Page - ${page}/${Math.round(data.length / 10 + 1)}` }
                   )
                   .setDescription(description)
               //   .setThumbnail(message.guild.iconURL({dynamic:true,size:4096}));
