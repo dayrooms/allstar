58c3d1bd1cd2b8483f2d3e62da3de9ca86c2c3ea
crime <139165956+dayrooms@users.noreply.github.com>
2026-09-09 01:04:50 -0500

    Update spotify.js

--- commands/spotify.js
+++ commands/spotify.js
@@ -1,5 +1,4 @@
-
-const{ MessageEmbed,MessageAttachment,MessageActionRow,MessageButton } = require('discord.js');
+const{ EmbedBuilder,AttachmentBuilder,ActionRowBuilder,ButtonBuilder,ButtonStyle } = require("discord.js");
 
 const canvacord = require("canvacord");
 const { default_prefix ,color,error,owner ,xmark} = require("../config.json")
@@ -28,11 +27,11 @@ module.exports = {
 let mentionedMember = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || client.users.cache.get(args[0])
 
 if(!mentionedMember) mentionedMember = message.guild.members.cache.get(message.author.id)
-            if(mentionedMember.presence == null) return message.reply({embeds:[new MessageEmbed().setDescription(`${xmark} That user is offline`).setColor(error)]})
+            if(mentionedMember.presence == null) return message.reply({embeds:[new EmbedBuilder().setDescription(`${xmark} That user is offline`).setColor(error)]})
       
       
-            if(mentionedMember.presence.activities[0] === 'undefined') return  message.reply({embeds:[new MessageEmbed().setDescription(`${xmark} That user is not listening to spotify`).setColor(error)]})
-           if(!mentionedMember.presence.activities[0]) return  message.reply({embeds:[new MessageEmbed().setDescription(`${xmark} That user is not listening to spotify`).setColor(error)]})
+            if(mentionedMember.presence.activities[0] === 'undefined') return  message.reply({embeds:[new EmbedBuilder().setDescription(`${xmark} That user is not listening to spotify`).setColor(error)]})
+           if(!mentionedMember.presence.activities[0]) return  message.reply({embeds:[new EmbedBuilder().setDescription(`${xmark} That user is not listening to spotify`).setColor(error)]})
             if(mentionedMember.presence.activities[0].name === 'Spotify') {
               
               
@@ -53,22 +52,22 @@ if(!mentionedMember) mentionedMember = message.guild.members.cache.get(message.a
                   .setTitle(trackName);
                card.build()
                    .then(data => {
-                    let attachment = new MessageAttachment(data,"spotify.png")
-            const embed = new MessageEmbed()
+                    let attachment = new AttachmentBuilder(data, { name: "spotify.png" })
+            const embed = new EmbedBuilder()
                 .setAuthor({name:`${mentionedMember.user.username} is listening `,iconURL:`${mentionedMember.user.displayAvatarURL({dynamic:true})}`})
-                .setColor("GREEN")
+                .setColor("Green")
                 .setImage('attachment://spotify.png')
                // .setThumbnail(trackIMG)
               //  .addField('Spotify <:sspotify:1019692790910820482> ', `> **Track** :: [${trackName}](${trackURL}) \n> **Artist** :: ${mentionedMember.presence.activities[0].state}\n> **Album** :: ${trackAlbum} `,true)    //\n> **Ends** :: ${ends}`, true)
             
-       const row = new MessageActionRow()
+       const row = new ActionRowBuilder()
 
        .addComponents(
-         new MessageButton()
+         new ButtonBuilder()
          .setLabel('Listen here')
          .setEmoji("<:sspotify:1019692790910820482> ")
          .setURL(`${trackURL}`)
-         .setStyle('LINK'),
+         .setStyle(ButtonStyle.Link),
         )
             message.reply({embeds:[embed],files:[attachment],components:[row]})
               .then(messageReact => {
@@ -77,7 +76,7 @@ if(!mentionedMember) mentionedMember = message.guild.members.cache.get(message.a
             }) 
                })
               
-            } else if(!mentionedMember.presence.activities[1]) return  message.reply({embeds:[new MessageEmbed().setDescription(`${xmark} That user is not listening to spotify`).setColor(error)]})
+            } else if(!mentionedMember.presence.activities[1]) return  message.reply({embeds:[new EmbedBuilder().setDescription(`${xmark} That user is not listening to spotify`).setColor(error)]})
               
              else if(mentionedMember.presence.activities[1].name === 'Spotify'){
               
@@ -99,21 +98,21 @@ if(!mentionedMember) mentionedMember = message.guild.members.cache.get(message.a
                   .setTitle(trackName);
                card.build()
                    .then(data => {
-                    let attachment = new MessageAttachment(data,"spotify.png")
-            const embed = new MessageEmbed()
+                    let attachment = new AttachmentBuilder(data, { name: "spotify.png" })
+            const embed = new EmbedBuilder()
                 .setAuthor({name:`${mentionedMember.user.username} is listening `,iconURL:`${mentionedMember.user.displayAvatarURL({dynamic:true})}`})
-                .setColor("GREEN")
+                .setColor("Green")
              .setImage('attachment://spotify.png')
                // .setThumbnail(trackIMG)
                // .addField('Spotify <:sspotify:1019692790910820482> ', `> **Track** :: [${trackName}](${trackURL}) \n> **Artist** :: ${mentionedMember.presence.activities[1].state}\n> **Album** :: ${trackAlbum}  `,true)    //\n> **Ends** :: ${ends}`, true)
-      const row = new MessageActionRow()
+      const row = new ActionRowBuilder()
 
        .addComponents(
-         new MessageButton()
+         new ButtonBuilder()
          .setLabel('Listen here')
          .setEmoji("<:sspotify:1019692790910820482> ")
          .setURL(`${trackURL}`)
-         .setStyle('LINK'),
+         .setStyle(ButtonStyle.Link),
         )
             message.reply({embeds:[embed],files:[attachment],components:[row]})
               .then(messageReact => {
@@ -130,7 +129,7 @@ if(!mentionedMember) mentionedMember = message.guild.members.cache.get(message.a
               
             }else{
               message.reply({embeds:[
-                new MessageEmbed()
+                new EmbedBuilder()
                 .setDescription(`${xmark} That user is not listening to spotify`)
                 .setColor(error)
               ]})
@@ -151,5 +150,3 @@ if(!mentionedMember) mentionedMember = message.guild.members.cache.get(message.a
     
 	},
 };
-
-
