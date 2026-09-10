1da987c8d3ac3ac742857065ecc411c7c68a6bb3
crime <139165956+dayrooms@users.noreply.github.com>
2026-09-10 01:16:17 -0500

    Update user.js

--- commands/user.js
+++ commands/user.js
@@ -1,5 +1,4 @@
-
-const{ MessageEmbed } = require('discord.js');
+const{ EmbedBuilder } = require('discord.js');
 const { default_prefix ,color,error,owner,xmark,checked } = require("../config.json")
 const db = require('quick.db')
 const talkedRecently = new Set();
