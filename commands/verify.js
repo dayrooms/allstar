3b747b15d20fe219260d6d954004afc2f36f29b4
crime <139165956+dayrooms@users.noreply.github.com>
2026-09-10 01:24:47 -0500

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -1,5 +1,4 @@
-
-const{ MessageEmbed,MessageActionRow,MessageButton,xmark  } = require('discord.js');
+const{ EmbedBuilder,ActionRowBuilder,ButtonBuilder,ButtonStyle  } = require('discord.js');
 const { default_prefix ,color,error,owner } = require("../config.json")
 const talkedRecently = new Set();
 module.exports = {
@@ -31,23 +30,23 @@ module.exports = {
       
 
 
-       const button = new MessageButton()
+       const button = new ButtonBuilder()
       .setCustomId('previousbtnas')
       .setEmoji("<:dick:1031696971053944832>")
-      .setStyle('SECONDARY');
+      .setStyle(ButtonStyle.Secondary);
 
 
 
     let buttonList = [
       button,
   ]
-    const row = new MessageActionRow().addComponents(buttonList);
-        let embed = new MessageEmbed()
+    const row = new ActionRowBuilder().addComponents(buttonList);
+        let embed = new EmbedBuilder()
         .setColor('#c495f0')
         .setDescription('<:04_Bblack:996539341096624168> click the button to verify \n\n inv [allstar](https://discord.com/oauth2/authorize?client_id=938863295543251024&permissions=8&scope=bot%20applications.commands) & [vile](https://discord.com/api/oauth2/authorize?client_id=991695573965099109&permissions=8&scope=bot%20applications.commands)')
         .setTitle('Welc 2 Heist')
         .setThumbnail('https://cdn.discordapp.com/attachments/1006892159477219428/1018227457561726997/cbe021ea5492cd53da0f9f8ee08acefb.jpg' )
-        .setFooter('inv allstar & vile')
+        .setFooter({ text: 'inv allstar & vile' })
    
 
      const curPage = await message.channel.send({
